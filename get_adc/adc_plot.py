24cbe1a66adb205b4b7be3784fbe19df7d721cbb
Ekaterina Tkachik <ekaterinatkachik@yandex.ru>
2026-03-02 11:53:00 +0300

    adc

---
+++ get_adc/adc_plot.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+
+def plot_voltage_vs_time(time, voltage, max_voltage):
+    plt.figure(figsize=(10,6))
+    plt.plot(time, voltage)
+    plt.title("График зависимости напряжения на входе АЦП от времени")
+    plt.xlim(0, 3)
+    plt.ylim(0, 3.5)
+    plt.grid()
+    plt.xlabel("Время, с")
+    plt.ylabel("Напряжение, В")
+    plt.show()
+
+def plot_sampling_period_hist(time):
+    sampling_periods = []
+    for i in range(1, len(time)):
+        period = time[i] - time[i-1]
+        sampling_periods.append(period)
+    plt.figure(figsize=(10,6))
+    plt.title("Распределене периодов дискретизации измерений по времени на одно измерение")
+    plt.hist(sampling_periods)
+    plt.xlim(0, 0.06)
+    plt.grid()
+    plt.xlabel("Период измерения, с")
+    plt.ylabel("Количество измерений")
+    plt.show()
